ee904db3676864fbc6f681b57c12da35353b6acf
Jeong Jiyong <98jiyong@gmail.com>
2024-10-28 17:26:04 +0900

    first commit

---
+++ www/src/main/java/www/silver/hom/HomeController.java
@@ -0,0 +1,45 @@
+package www.silver.hom;
+
+import java.text.DateFormat;
+import java.util.Date;
+import java.util.Locale;
+
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
+
+/** aaa
+ * Handles requests for the application home page.
+ */
+@Controller
+// 클라이언트 요청 처리는 컨트롤러가 한다.
+// 컨트롤러는 @Controller를 붙여 사용한다.
+public class HomeController {
+	
+	private static final Logger logger = LoggerFactory.getLogger(HomeController.class);
+	
+	/**
+	 * Simply selects the home view to render by returning its name.
+	 */
+	@RequestMapping(value = "/", method = RequestMethod.GET)
+	// value값인 /를 받아 실행
+	public String home(Locale locale, Model model) {
+		logger.info("Welcome home! The client locale is {}.", locale);
+		
+		Date date = new Date();
+		DateFormat dateFormat = DateFormat.getDateTimeInstance(DateFormat.LONG, DateFormat.LONG, locale);
+		
+		String formattedDate = dateFormat.format(date);
+		
+		model.addAttribute("serverTime", formattedDate );
+		
+		return "home";
+		// 리턴 타입은 .jsp이며 사용할땐 파일명만 쓴다.
+	}
+	
+	//https://bubblecastle.tistory.com/9
+	
+}
